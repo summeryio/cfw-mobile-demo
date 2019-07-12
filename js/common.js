760b6440758604ce9ba1fdb3e86a5a5abfffd9f5
summeryio <1005526074@qq.com>
2019-07-12 18:16:41 +0800

    init

---
+++ js/common.js
@@ -0,0 +1,66 @@
+window.addEventListener( "load", function() {
+	FastClick.attach( document.body );
+}, false );
+
+// 显示隐藏导航
+var $m_mask = $('#m_mask')
+
+$(document).on('click', '.m-header .icon-nav-extension', function (ev) {
+	ev.stopPropagation()
+	ev.preventDefault()
+	
+	$m_mask.show()
+	
+
+	if (!$('.header-popup').hasClass('showing')) {
+		$('.header-popup').addClass('showing')
+		$(document.body).addClass('no-scroll')
+	} else {
+		$('.header-popup').removeClass('showing')
+		$(document.body).removeClass('no-scroll')
+		$m_mask.hide()
+	}
+
+	$('#category_nav').find('.nav li').removeClass('active')
+	$('#category_nav').find('.cont li').removeClass('on')
+
+	$('#category_nav').find('.cont-wrap').hide()
+})
+
+// 点击遮罩隐藏导航
+$m_mask.on('click', function (ev) {
+	$(this).hide()
+	$(document.body).removeClass('no-scroll')
+	$('.header-popup').removeClass('showing')
+	$('#category_nav .cont-wrap').hide()
+
+	$('#category_nav').find('.nav li').removeClass('active')
+	$('#category_nav').find('.cont li').removeClass('on')
+
+	ev.stopPropagation();
+	ev.preventDefault();
+})
+
+
+// 返回上一页
+$(document).on('click', '.m-header .icon-left', function () {
+	window.history.back(-1)
+})
+
+
+// 图片懒加载
+$("img.lazy").lazyload({
+	threshold :50,
+	placeholder: '/tpl/wap2020/images/img_default.png'
+});
+var loadIMGErr = function (_this) {
+	_this.src = '/tpl/wap2020/images/img_default.png'
+}
+
+
+// 电话拨打记录
+function submitPhone(cat, phone, title) {
+	$.post("/tellog.php",{'categoryid': cat,'title': title + phone},function(result){
+		return true;
+	});
+}
---
+++ js/util.js
@@ -0,0 +1,68 @@
+var util = {
+    /**
+     * 视图滑动并加载数据，添加懒加载
+     * @param {pageCount} 数据的页数 
+     * @param {path} 数据引用的路径  
+     */
+    dropLoadData: function (pageCount, path) {
+        var counter = 0;
+        var num = 10; // 每页展示几个
+        var pageStart = 2,pageEnd = 0;
+        var page=2;
+
+        $("img.lazy").lazyload({
+            threshold :100,
+            placeholder: '/tpl/wap2019/images/img_default.png'
+        });
+        
+            
+        if(pageCount > 1) {
+            $('.load-wrapper').dropload({
+                scrollArea : window,
+                domDown : {
+                    domClass   : 'dropload-down',
+                    domRefresh : '<div class="dropload-refresh">↑上拉加载更多</div>',
+                    domLoad    : '<div class="dropload-load"><span class="loading"></span>加载中...</div>',
+                    domNoData  : '<div class="dropload-noData">- 我是有底线的 -</div>'
+                },
+                loadDownFn : function(me){
+                    console.log('data')
+                    var template = '';
+                    counter++;
+                    pageEnd = num * counter;
+                    pageStart = pageEnd - num;
+
+                    // for(var i = pageStart; i < pageEnd; i++){
+                        $.get("/src/inc/"+path+"",{'pages':page}, function(result){
+                            if(result=='0'){
+                                return;
+                            }
+                            template = result
+
+                            if(page >= pageCount) {
+                                me.lock();
+                                me.noData();
+                                return;
+                            }
+                            page=page+1;      
+                        }); 
+                    // }
+
+                    setTimeout(function(){
+                        $('.load-wrapper .list').append(template);
+                        $("img.lazy").lazyload({
+                            threshold :100,
+                            placeholder: '/tpl/wap2019/images/img_default.png'
+                        });
+                        // 每次数据加载完，必须重置
+                        me.resetload();
+                    },1000);
+                },
+                threshold : 50
+            });
+        } else {
+            console.log('no-data')
+            $('.dropload-down').hide()
+        }
+    }
+}
